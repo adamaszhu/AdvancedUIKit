a0f5b7665c1899c626ce40b5dab762271173ffb6
Adamas Zhu <adamas.zhu@hotmail.com>
2017-04-22 19:34:29 +1000

    Improve String+Display functions

---
+++ AdvancedUIKit/Internal/String+Helper.swift
@@ -0,0 +1,37 @@
+/**
+ * String+Helper provides additional functions for a string object.
+ * - author: Adamas
+ * - version: 1.0.0
+ * - date: 20/04/2017
+ */
+extension String {
+    
+    /**
+     * Remove a specific suffix from the string.
+     * - parameter suffix: The suffix to be removed.
+     * - returns: The suffix removed string. Nil if the suffix doesn't exist in the original string.
+     */
+    func removeSuffix(_ suffix: String) -> String? {
+        if !hasSuffix(suffix) {
+            return nil
+        }
+        let suffixBeginIndex = index(endIndex, offsetBy: -suffix.characters.count)
+        return substring(to: suffixBeginIndex)
+    }
+    
+    /**
+     * Remove a specific prefix from the string.
+     * - parameter prefix: The prefix to be removed.
+     * - returns: The prefix removed string. Nil if the prefix doesn't exist in the original string.
+     */
+    func removePrefix(_ prefix: String) -> String? {
+        if !hasPrefix(prefix) {
+            return nil
+        }
+        let prefixEndIndex = index(startIndex, offsetBy: prefix.characters.count)
+        return substring(from: prefixEndIndex)
+    }
+    
+}
+
+import Foundation
--- AdvancedUIKit/String/String+Display.swift
+++ AdvancedUIKit/String/String+Display.swift
@@ -8,34 +8,86 @@
 extension String {
     
     /**
+     * Get the width of a string with a specific font.
+     * - parameter font: The font.
+     * - returns: The width.
+     */
+    func measureWidth(withFont font: UIFont) -> CGFloat {
+        return NSString(string: self).size(attributes: [NSFontAttributeName: font]).width
+    }
+    
+    /**
+     * Get the actual line amount displayed on the screen.
+     * - parameter font: The font that is applied.
+     * - parameter view: The view that text is in.
+     * - returns: The actual line amount displayed on the view.
+     */
+    func measureLineAmount(withFont font: UIFont, inView view: UIView) -> Int {
+        return measureLines(withFont: font, inView: view).count
+    }
+    
+    /**
      * Get the actual lines displayed on the screen.
      * - parameter font: The font that is applied.
      * - parameter view: The view that text is in.
      * - returns: The actual lines displayed on the view.
      */
-    func measureLineAmount(withFont font: UIFont, inView view: UIView) -> Int {
+    func measureLines(withFont font: UIFont, inView view: UIView) -> [String] {
         if view.frame.width == 0 {
-            return 0
+            return []
         }
-        if isEmpty {
-            return 1
-        }
-        let viewWidth = view.frame.width
-        var lineAmount = 0
+        var displayedLines = [String]()
         let lines = components(separatedBy: CharacterSet.newlines)
         for line in lines {
-            let size = NSString(string: line).size(attributes: [NSFontAttributeName: font])
-            if size.width == 0 {
-                // COMMENT: An empty line.
-                lineAmount = lineAmount + 1
+            for displayedLine in line.measureSingleLine(withFont: font, inView: view) {
+                displayedLines.append(displayedLine)
+            }
+        }
+        return displayedLines
+    }
+    
+    /**
+     * Measure lines displayed on the screen for a single line according to the font and view contains it.
+     * - parameter font: The font that is applied.
+     * - parameter view: The view that text is in.
+     * - returns: The actual line amount displayed on the view.
+     */
+    private func measureSingleLine(withFont font: UIFont, inView view: UIView) -> [String] {
+        if isEmpty {
+            return [""]
+        }
+        var lines = [String]()
+        var remainLine = self
+        while !remainLine.isEmpty {
+            // COMMENT: Extract a line from the beginning of the remainLine. For better performance, words seperating strategy is considered prior to character seperating strategy.
+            // COMMENT: Extract a line containing several words from the beginning of the remainLine.
+            var words = remainLine.components(separatedBy: CharacterSet.whitespaces)
+            var width = words.joined(separator: " ").measureWidth(withFont: font)
+            while (width > view.frame.width) && (words.count > 1) {
+                words.removeLast()
+                width = words.joined(separator: " ").measureWidth(withFont: font)
+            }
+            if width <= view.frame.width {
+                let line = words.joined(separator: " ")
+                lines.append(line)
+                // COMMENT: remainLine must have the line extracted.
+                remainLine = remainLine.removePrefix(line)!
+                // COMMENT: If the remainLine is not empty, remove the space between current line and the next line.
+                remainLine = remainLine.removePrefix(" ") ?? remainLine
                 continue
             }
-            lineAmount = lineAmount + Int(size.width / viewWidth)
-            // COMMENT: Whether the last line is a full line or half line.
-            let isLastLineFilled = Int(size.width) % Int(viewWidth) == 0
-            lineAmount = lineAmount + (isLastLineFilled ? 0 : 1)
+            // COMMENT: Extra a line from a extra long word from the beginning of the remainLine. Only one word is left.
+            var word = words.first!
+            width = word.measureWidth(withFont: font)
+            while width > view.frame.width {
+                word = word.substring(to: word.index(word.endIndex, offsetBy: -1))
+                width = word.measureWidth(withFont: font)
+            }
+            lines.append(word)
+            // COMMENT: remainLine must have the line extracted.
+            remainLine = remainLine.removePrefix(word)!
         }
-        return lineAmount
+        return lines
     }
     
 }
