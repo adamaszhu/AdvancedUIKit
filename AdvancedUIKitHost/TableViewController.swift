ddeee268589d788f5378d860f107cf92b036d115
Adamas Zhu <adamas.zhu@hotmail.com>
2017-04-22 11:54:31 +1000

    Add basic structure for view related classes

---
+++ AdvancedUIKitHost/TableViewController.swift
@@ -0,0 +1,10 @@
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.contentInset.top = 20
+    }
+    
+}
+
+import UIKit
