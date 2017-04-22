e19c0324a60e6b4bcfbeb82b92cdda6841ee3b7e
Adamas Zhu <adamas.zhu@hotmail.com>
2017-04-22 16:05:23 +1000

    Change font support to string support

--- AdvancedUIKit/String/String+Display.swift
+++ AdvancedUIKit/String/String+Display.swift
@@ -1,27 +1,30 @@
 /**
- * Font+Measure add additional support for the font to measure texts on the screen.
+ * String+Display add additional support for displaying a string on the screen.
  * - author: Adamas
  * - version: 1.0.0
  * - date: 22/04/2017
  */
 
-extension UIFont {
+extension String {
     
     /**
-     * Measure how many lines the context based on the font.
+     * Get the actual lines displayed on the screen.
+     * - parameter font: The font that is applied.
+     * - parameter view: The view that text is in.
+     * - returns: The actual lines displayed on the view.
      */
-    func measureLineAmount(ofContext context: String, inView view: UIView) -> Int {
+    func measureLineAmount(withFont font: UIFont, inView view: UIView) -> Int {
         if view.frame.width == 0 {
             return 0
         }
-        if context.isEmpty {
+        if isEmpty {
             return 1
         }
         let viewWidth = view.frame.width
         var lineAmount = 0
-        let lines = context.components(separatedBy: CharacterSet.newlines)
+        let lines = components(separatedBy: CharacterSet.newlines)
         for line in lines {
-            let size = NSString(string: line).size(attributes: [NSFontAttributeName: self])
+            let size = NSString(string: line).size(attributes: [NSFontAttributeName: font])
             if size.width == 0 {
                 // COMMENT: An empty line.
                 lineAmount = lineAmount + 1
