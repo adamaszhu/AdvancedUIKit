2706db90bcda14973ed0e2194f1e93916cc9cf98
Adamas Zhu <adamas.zhu@hotmail.com>
2017-05-29 23:25:27 +1000

    Improve the DeviceHelper

---
+++ AdvancedUIKit/Device/DeviceHelper.swift
@@ -0,0 +1,129 @@
+/**
+ * DeviceHelper is used to perform an user interaction. Such as sending an email or making a phone call.
+ * - version: 0.2.0
+ * - date: 17/02/2017
+ */
+public class DeviceHelper: NSObject {
+    
+    /**
+     * The singleton instance in the system.
+     */
+    public static let shared: DeviceHelper = DeviceHelper()
+    
+    /**
+     * User error.
+     */
+    private let dialError = "DialError"
+    private let mapError = "MapError"
+    
+    /**
+     * System error.
+     */
+    private let phoneNumberError = "The phone number is invalid."
+    private let addressError = "The address is incorrect."
+//    private let rootViewError = "There is no root view."
+    
+    /**
+     * Function url.
+     */
+    private let dailPrefix = "telprompt://"
+    private let mapPrefix = "http://maps.apple.com/?q="
+    
+    /**
+     * The delegate of the DeviceHelper.
+     */
+    public var deviceHelperDelegate: DeviceHelperDelegate?
+    
+    /**
+     * The application used to do the action.
+     */
+    private let application: UIApplication
+    
+    /**
+     * Make a phone call.
+     * - parameter number: The phone number.
+     */
+    public func dial(withNumber number: String) {
+        guard let dialURL = URL(string: "\(dailPrefix)\(number)") else {
+            Logger.standard.logInfo(phoneNumberError, withDetail: number)
+            return
+        }
+        open(dialURL, withError: dialError)
+    }
+    
+    /**
+     * Show a location in the map application.
+     * - parameter address: The address to be shown.
+     */
+    public func showMap(withAddress address: String) {
+        let formattedAddress = address.replacingOccurrences(of: " ", with: "+")
+        guard let mapURL = URL(string: "\(mapPrefix)\(formattedAddress)") else {
+            Logger.standard.logInfo(addressError, withDetail: address)
+            return
+        }
+        open(mapURL, withError: mapError)
+    }
+//
+//    /**
+//     * Send an email.
+//     * - parameter address: The email address.
+//     * - parameter subject: The subject of the email.
+//     * - parameter content: The content of the email.
+//     * - parameter isHTMLContent: Whether the content is a html or not.
+//     * - parameter attachmentList: A list of attachments of the email.
+//     */
+//    public func sendEmail(toAddress address: String, withSubject subject: String, withContent content: String, withAttachments attachmentList: Dictionary<String, NSData>? = nil, asHTMLContent isHTML: Bool = false) {
+//        let mailViewController = MFMailComposeViewController()
+//        mailViewController.mailComposeDelegate = self
+//        mailViewController.setToRecipients([address])
+//        mailViewController.setSubject(subject)
+//        if attachmentList != nil {
+//            for attachmentName in attachmentList!.keys {
+//                let fileInfoAccessor = FileInfoAccessor(withPath: attachmentName)
+//                if fileInfoAccessor.mimeType == nil {
+//                    continue
+//                }
+//                mailViewController.addAttachmentData(attachmentList![attachmentName]!, mimeType: fileInfoAccessor.mimeType!, fileName: attachmentName);
+//            }
+//        }
+//        mailViewController.setMessageBody(content, isHTML: isHTML)
+//        var rootViewController = application.keyWindow?.rootViewController
+//        if rootViewController == nil {
+//            logError(ContactHelper.RootViewError)
+//            return
+//        }
+//        if rootViewController!.isKindOfClass(UINavigationController) {
+//            let navigationController = rootViewController as! UINavigationController
+//            rootViewController = navigationController.viewControllers.last
+//        }
+//        // TODO: Consider about the sliding view.
+//        rootViewController?.presentViewController(mailViewController, animated: true, completion: nil);
+//    }
+//    
+//    
+    /**
+     * Open an URL.
+     * - parameter url: The url to be opened.
+     * - parameter message: The message to be performed if the URL is not available.
+     */
+    private func open(_ url: URL, withError error: String) {
+        guard application.canOpenURL(url) else {
+            deviceHelperDelegate?.deviceHelper(self, didCatchError: error.localizeWithinFramework(forType: DeviceHelper.self))
+            return
+        }
+        application.open(url, options: [:], completionHandler: nil)
+    }
+    
+    /**
+     * Initialize the object.
+     * - parameter application: The application used to make a function call.
+     */
+    init(application: UIApplication = UIApplication.shared) {
+        self.application = application
+        super.init()
+    }
+    
+}
+
+import Foundation
+import MessageUI
---
+++ AdvancedUIKit/Device/DeviceHelperDelegate+Optional.swift
@@ -0,0 +1,14 @@
+/**
+ * DeviceHelper+Optional implements the default action of some DeviceHelperDelegate actions.
+ * - author: Adamas
+ * - version: 1.0.0
+ * - date: 29/05/2017
+ */
+extension DeviceHelperDelegate {
+    
+    /**
+     * DeviceHelperDelegate
+     */
+    func deviceHelper(_ deviceHelper: DeviceHelper, didSendEmail result: Bool) { }
+    
+}
