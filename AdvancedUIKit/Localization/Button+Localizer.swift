ff660781ac7dd76865afcc9def52205c2c99c485
Adamas Zhu <adamas.zhu@hotmail.com>
2017-06-02 23:46:45 +1000

    Add localisation for label, button and segmented control

---
+++ AdvancedUIKit/Localization/Button+Localizer.swift
@@ -0,0 +1,21 @@
+/**
+ * Button+Localizer provides localization support for a button.
+ * - author: Adamas
+ * - version: 1.0.0
+ * - date: 02/06/2017
+ */
+public extension UIButton {
+    
+    /**
+     * UIView
+     */
+    public override func localize(withLocalizationFile localizationFile: String = defaultLocalizationFile) {
+        guard let title = title else {
+            return
+        }
+        self.title = title.localize(withLocalizationFile: localizationFile)
+    }
+    
+}
+
+import UIKit
