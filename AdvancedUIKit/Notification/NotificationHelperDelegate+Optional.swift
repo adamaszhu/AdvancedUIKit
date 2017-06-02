4acf501502119f7210611d831585fd7c3399bf4c
Adamas Zhu <adamas.zhu@outware.com.au>
2017-06-02 16:53:14 +1000

    Import old notification implementation

---
+++ AdvancedUIKit/Notification/NotificationHelperDelegate+Optional.swift
@@ -0,0 +1,19 @@
+/**
+ * NotificationHelperDelegate+Optional implements the default action for the delegate.
+ * - author: Adamas
+ * - version: 1.0.0
+ * - date: 02/06/2017
+ */
+public extension NotificationHelperDelegate {
+    
+    /**
+     * NotificationHelperDelegate
+     */
+    func notificationHelper(_ notificationHelper: NotificationHelper, didAuthorizeRemoteNotification result: Bool) { }
+    
+    /**
+     * NotificationHelperDelegate
+     */
+    func notificationHelper(_ notificationHelper: NotificationHelper, didAuthorizeLocalNotification result: Bool) {}
+    
+}
