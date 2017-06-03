7268860dfc7fa883bde0ebda273452c271129c2f
Adamas Zhu <adamas.zhu@hotmail.com>
2017-06-03 17:58:26 +1000

    Fix the notification using the background temp thread

--- AdvancedUIKitHost/View/Notification/NotificationViewController.swift
+++ AdvancedUIKitHost/View/Notification/NotificationViewController.swift
@@ -2,6 +2,8 @@ class NotificationViewController: UIViewController {
     
     private let notificationHelper: NotificationHelper
     
+    private var backgroundTask: UIBackgroundTaskIdentifier = UIBackgroundTaskInvalid
+    
     required init?(coder aDecoder: NSCoder) {
         notificationHelper = NotificationHelper.shared
         super.init(coder: aDecoder)
@@ -14,9 +16,16 @@ class NotificationViewController: UIViewController {
     }
     
     @IBAction func showLocalNotification(_ sender: Any) {
+        backgroundTask = UIApplication.shared.beginBackgroundTask()
+        showLocalNotification()
+    }
+    
+    func showLocalNotification() {
         let dispatchTime = DispatchTime.now() + 5
         DispatchQueue.main.asyncAfter(deadline: dispatchTime) { [unowned self] _ in
             self.notificationHelper.createLocalNotification(withTitle: "Title", withContent: "Content")
+            UIApplication.shared.endBackgroundTask(self.backgroundTask)
+            self.backgroundTask = UIBackgroundTaskInvalid
         }
     }
     
