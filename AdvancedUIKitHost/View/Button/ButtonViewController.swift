45069042bb736ef9755af826dffa17cfd4401b2f
Adamas Zhu <adamas.zhu@outware.com.au>
2017-09-15 09:00:40 +1000

    Update the button screen

--- AdvancedUIKitHost/View/Button/ButtonViewController.swift
+++ AdvancedUIKitHost/View/Button/ButtonViewController.swift
@@ -1,12 +1,17 @@
-class ButtonViewController: UIViewController {
+final class ButtonViewController: UIViewController {
     
-    private let buttonTitle = "Button\nTitle"
+    static private let buttonTitle = "Button\nTitle"
     
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet private weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.title = buttonTitle
+        button.title = ButtonViewController.buttonTitle
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        button.layer.cornerRadius = button.actualHeight
     }
     
 }
