92b234f38760c409ff6453e5f8cbb42f65a507d7
Adamas Zhu <adamas.zhu@outware.com.au>
2017-09-20 20:00:47 +1000

    Create the navigation screen

--- AdvancedUIKitHost/View/Navigation/NavigationViewController.swift
+++ AdvancedUIKitHost/View/Navigation/NavigationViewController.swift
@@ -1,6 +1,8 @@
-class NavigationViewController: UIViewController {
+final class NavigationViewController: UIViewController {
     
     private let navigation = (title: "Navigation", leftButtonName: "Back", rightButtonName: "Done")
+    private let navigationColor = UIColor.red
+    private let navigationTintColor = UIColor.white
     private let doneMessage = "Click the done button."
     
     @IBOutlet weak var navigationBar: NavigationBar!
@@ -8,11 +10,28 @@ class NavigationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBar.viewController = self
+        navigationBar.tintColor = navigationTintColor
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: navigationTintColor]
+        navigationBar.backgroundColor = navigationColor
         navigationBar.title = navigation.title
         navigationBar.rightButtonTitle = navigation.rightButtonName
         navigationBar.leftButtonTitle = navigation.leftButtonName
         navigationBar.setLeftButtonAction(action: #selector(NavigationBar.back), withTarget: navigationBar)
         navigationBar.setRightButtonAction(action: #selector(done), withTarget: self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationBar.animate(withChange: { [unowned self] _ in
+            self.navigationController?.navigationBar.alpha = 0
+            }, withCompletion: { [unowned self] _ in
+                self.navigationController?.isNavigationBarHidden = true
+        })
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
     }
     
     func done() {
