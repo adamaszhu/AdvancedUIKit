f06a12cf3bb07183187516f9fe81e755a45753e1
Adamas Zhu <account@adamaszhu.com>
2017-09-21 08:37:08 +1000

    Update picker screen

--- AdvancedUIKitHost/View/Picker/PickerViewController.swift
+++ AdvancedUIKitHost/View/Picker/PickerViewController.swift
@@ -1,7 +1,8 @@
-class PickerViewController: UIViewController {
+final class PickerViewController: UIViewController {
     
-    private let countries = ["China", "Australia", "America"]
-    private let country = "Country"
+    private let pickerOptions = ["China", "Australia", "America"]
+    private let pickerTitle = "Country"
+    private let pickerTitleColor = UIColor(red: 125 / 255, green: 182 / 255, blue: 216 / 255, alpha: 1)
     
     @IBOutlet weak var showDataPickerButton: UIButton!
     @IBOutlet weak var dataPicker: DataPicker!
@@ -9,11 +10,11 @@ class PickerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         dataPicker.dataPickerDelegate = self
-        let items = countries.map { (country) -> DataPickerItem in
-            DataPickerItem(value: country)
+        let items = pickerOptions.map {
+            DataPickerItem(value: $0)
         }
-        dataPicker.title = country
-        dataPicker.titleBackgroundColor = UIColor.brown
+        dataPicker.title = pickerTitle
+        dataPicker.titleBackgroundColor = pickerTitleColor
         dataPicker.setSingleColumn(items)
         if let title = showDataPickerButton.titleLabel?.text {
             dataPicker.selectValue(title)
