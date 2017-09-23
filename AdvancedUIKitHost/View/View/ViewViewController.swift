b500e5fa02b1cc2993c92e27663aee80f3fc5f17
Adamas Zhu <account@adamaszhu.com>
2017-09-23 14:56:15 +1000

    Update the view screen

--- AdvancedUIKitHost/View/View/ViewViewController.swift
+++ AdvancedUIKitHost/View/View/ViewViewController.swift
@@ -1,7 +1,7 @@
 final class ViewViewController: UIViewController {
     
-    let popupViewBackgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.8)
-    let animationOffset = CGFloat(100)
+    let popupViewBackgroundColor = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 0.4)
+    let animationOffset = CGFloat(200)
     
     var animationButtonOriginalFrame: CGRect!
     var popupView = PopupView()
@@ -10,11 +10,8 @@ final class ViewViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        popupView.frame = view.bounds
-        popupView.backgroundColor = popupViewBackgroundColor
-        let tapGesture = UITapGestureRecognizer(target: popupView, action: #selector(PopupView.hide))
-        popupView.addGestureRecognizer(tapGesture)
-        animationButtonOriginalFrame = animationButton.frame
+        configPopupView()
+        configAnimationView()
     }
     
     @IBAction func popup(_ sender: Any) {
@@ -23,7 +20,7 @@ final class ViewViewController: UIViewController {
     
     @IBAction func animate(_ sender: Any) {
         animationButton.animate(withChange: { [unowned self] _ in
-            self.animationButton.frame.origin = .init(x: self.animationButtonOriginalFrame.origin.x, y: self.animationButtonOriginalFrame.origin.y + self.animationOffset)
+            self.animationButton.frame.origin = .init(x: self.animationButtonOriginalFrame.origin.x, y: self.animationButtonOriginalFrame.origin.y - self.animationOffset)
         }, withPreparation: { [unowned self] _ in
             self.animationButton.frame = self.animationButtonOriginalFrame
         }) { [unowned self] _ in
@@ -31,6 +28,17 @@ final class ViewViewController: UIViewController {
                 self.animationButton.frame = self.animationButtonOriginalFrame
             })
         }
+    }
+    
+    private func configPopupView() {
+        popupView.frame = view.bounds
+        popupView.backgroundColor = popupViewBackgroundColor
+        let tapGesture = UITapGestureRecognizer(target: popupView, action: #selector(PopupView.hide))
+        popupView.addGestureRecognizer(tapGesture)
+    }
+    
+    private func configAnimationView() {
+        animationButtonOriginalFrame = animationButton.frame
     }
     
 }
