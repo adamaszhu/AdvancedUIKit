9892e799f706291d25bc21c17a6da79202bf1d1e
Adamas Zhu <account@adamaszhu.com>
2018-01-17 10:50:34 +1100

    Create notification without title

--- AdvancedUIKit/Notification/NotificationHelper.swift
+++ AdvancedUIKit/Notification/NotificationHelper.swift
@@ -1,8 +1,8 @@
 /// NotificationHelper is used to manage the notification.
 ///
 /// - author: Adamas
-/// - version: 1.1.0
-/// - date: 30/11/2017
+/// - version: 1.1.1
+/// - date: 17/01/2018
 final public class NotificationHelper {
     
     /// The singleton helper.
@@ -37,7 +37,7 @@ final public class NotificationHelper {
     ///   - delay: Delay in second for showing the notification
     ///   - soundName: The name of the sound.
     @available(iOS, introduced: 8.0, deprecated: 10.0, message: "Use UserNotifications Framework's UNAuthorizationOptions")
-    public func createLocalNotification(withTitle title: String, withContent content: String, withDelay delay: Double = 0, withSoundName soundName: String = UILocalNotificationDefaultSoundName) {
+    public func createLocalNotification(withTitle title: String? = nil, withContent content: String, withDelay delay: Double = 0, withSoundName soundName: String = UILocalNotificationDefaultSoundName) {
         if !isLocalNotificationAuthorized {
             notificationHelperDelegate?.notificationHelper(self, didCatchError: NotificationHelper.authorizationError.localizedInternalString(forType: NotificationHelper.self))
             return
@@ -45,7 +45,7 @@ final public class NotificationHelper {
         let notification = UILocalNotification()
         notification.timeZone = NSTimeZone.default
         notification.fireDate = Date().addingTimeInterval(delay)
-        if #available(iOS 8.2, *) {
+        if #available(iOS 8.2, *), let title = title {
             notification.alertTitle = title
         }
         notification.alertBody = content
