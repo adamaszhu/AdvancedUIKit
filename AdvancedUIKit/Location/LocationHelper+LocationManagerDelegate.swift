9e2ffa75364aafc0eeb980d8881f64ee560fd1f9
Adamas Zhu <account@adamaszhu.com>
2018-07-03 11:06:11 +1000

    Fix a bug in LocationHelper

--- AdvancedUIKit/Location/LocationHelper+LocationManagerDelegate.swift
+++ AdvancedUIKit/Location/LocationHelper+LocationManagerDelegate.swift
@@ -1,11 +1,14 @@
 /// LocationHelper+LocationManagerDelegate wrap the action of a CLLocationManagerDelegate
 ///
 /// - author: Adamas
-/// - version: 1.0.0
-/// - date: 04/05/2017
+/// - version: 1.3.0
+/// - date: 02/07/2018
 extension LocationHelper: CLLocationManagerDelegate {
     
     public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status != .notDetermined else {
+            return
+        }
         switch authorizingStatus {
         case .authorizedAlways:
             locationHelperDelegate?.locationHelper(self, didAuthorizeAlwaysAuthorization: isAlwaysAuthorizationAuthorized)
