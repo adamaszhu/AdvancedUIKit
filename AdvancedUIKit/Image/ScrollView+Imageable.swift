0c5e42c611652c88cd7f5e340f6da3a3d5306cf7
Adamas <azhu@transurban.com>
2021-03-28 09:31:59 +1100

    Update the screenshot for a scroll view

--- AdvancedUIKit/Image/ScrollView+Imageable.swift
+++ AdvancedUIKit/Image/ScrollView+Imageable.swift
@@ -7,21 +7,35 @@ public extension UIScrollView {
     
     /// The screenshot created for the view.
     ///
+    ///  - Important:
+    ///  Since iOS 13, cells out of the view are not persistent in the memory. So UITableView and UICollectionView cannot generate screenshots.
+    ///
     /// - Parameter scale: The scale of the screenshot
     /// - Returns: The screenshot
     func contentScreenshot(withScale scale: Double = 1) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(contentSize, isOpaque, CGFloat(scale))
-        guard let context = UIGraphicsGetCurrentContext() else {
-            Logger.standard.logError(Self.contextError)
+        guard #available(iOS 13, *) else {
+            UIGraphicsBeginImageContextWithOptions(contentSize, isOpaque, CGFloat(scale))
+            guard let context = UIGraphicsGetCurrentContext() else {
+                Logger.standard.logError(Self.contextError)
+                return nil
+            }
+            let previousFrame = frame
+            frame = CGRect(origin: frame.origin, size: contentSize)
+            layer.render(in: context)
+            frame = previousFrame
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image
+        }
+        if self is UITableView || self is UICollectionView {
+            Logger.standard.logError(Self.typeError)
             return nil
         }
-        let previousFrame = frame
-        frame = CGRect(origin: frame.origin, size: contentSize)
-        layer.render(in: context)
-        frame = previousFrame
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image
+        if let contentView = subviews.first {
+            return contentView.screenshot(withScale: scale)
+        } else {
+            return nil
+        }
     }
 }
 
@@ -30,6 +44,7 @@ private extension UIScrollView {
     
     /// System errors
     static let contextError = "Graphics context is nil"
+    static let typeError = "Table views and collection views cannot generate screenshots"
 }
 
 import AdvancedFoundation
