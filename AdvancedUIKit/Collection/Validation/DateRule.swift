724e11515e8ad3baaf9828daa9947819697fbd66
Adamas Zhu <developer@adamaszhu.com>
2022-09-14 22:39:04 +1000

    Add DateRule

---
+++ AdvancedUIKit/Collection/Validation/DateRule.swift
@@ -0,0 +1,35 @@
+/// DateRule presents the date requirement that a date string should follow.
+///
+/// - version: 1.9.6
+/// - date: 14/09/22
+/// - author: Adamas
+public class DateRule: RuleType {
+
+    /// The error message to return if the validation fails.
+    private let message: String
+
+    /// Supported date formats
+    private let dateFormats: [DateFormat]
+
+    /// Initialize the rule
+    /// - Parameters:
+    ///   - dateFormats: A list of format supported by the rule
+    ///   - message: The error message to apply.
+    public init(dateFormats: [DateFormat], message: String) {
+        self.dateFormats = dateFormats
+        self.message = message
+    }
+
+    public func isValid(value: String?) -> String? {
+        let value = value ?? String()
+        for dateFormat in dateFormats {
+            if Date(string: value, dateFormat: dateFormat) != nil {
+                return nil
+            }
+        }
+        return message
+    }
+}
+
+import AdvancedFoundation
+import Foundation
